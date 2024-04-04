6bb01a305b059393f75d8ea07694ce77205f64d2
David Anderson <davidanderson@live.ie>
2024-04-04 18:42:00 +0000

    Set DEBUG = True

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gy+#ncgb#bdtan=#-jl1v#&p2!r7@)-#40o_jd2qb!7p$3zm=h'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-01000100010-djangoproje-etj4its2fha.ws-eu110.gitpod.io',
                 '.heroku.app']
